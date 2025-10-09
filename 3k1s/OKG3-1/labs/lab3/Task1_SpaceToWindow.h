56146511649638766754a44e05fc87f02886a6b2
Vi <98347065+cyclikya@users.noreply.github.com>
2025-10-09 18:22:47 +0300

    09.10

--- 3k1s/OKG3-1/labs/lab3/Task1_SpaceToWindow.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CMatrix.h"
-#include "CRectD.h"
-#include <afxwin.h>
-
-// ========== ЗАДАНИЕ 1 ==========
-// Функция пересчёта координат из мировой системы в оконную
-CMatrix SpaceToWindow(CRectD& rs, CRect& rw);
