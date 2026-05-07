4cd49870384f153f5c5653022cc938ca4626fe99
Vi <98347065+cyclikya@users.noreply.github.com>
2026-05-07 10:56:05 +0300

    07.05

--- 3k2s/MSHOAD3-2/scripts(SQLServer)/lab10.sql
+++ 3k2s/MSHOAD3-2/scripts(SQLServer)/lab10.sql
@@ -1,62 +1,83 @@
 USE master;
-GO
-
+-- Включение CLR
 EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
-GO
 EXEC sp_configure 'clr enabled', 1;
 RECONFIGURE;
-GO
 EXEC sp_configure 'clr strict security', 0;
 RECONFIGURE;
 GO
+Create database lab10;
 
+-- Разрешение для сборки с доступом к внешнему файлу
 ALTER DATABASE [lab10] SET TRUSTWORTHY ON;
 GO
 
 USE [lab10];
 GO
 
-DROP PROCEDURE IF EXISTS dbo.MoveFile;
-GO
-
-DROP TYPE IF EXISTS dbo.CurrencyExchange;
-GO
-
+-- Удаление старых объектов, если они уже существуют
+DROP PROCEDURE IF EXISTS dbo.ReadLicenseFile;
+DROP TYPE IF EXISTS dbo.LicenseData;
 DROP ASSEMBLY IF EXISTS lab10;
 GO
 
+-- Подключение CLR-сборки
 CREATE ASSEMBLY lab10
-FROM 'D:\BGTU\VI sem\MCXOiAD\lab10\lab10\bin\Debug\lab10.dll'
+FROM 'C:\General\BELSTU\3k2s\MSHOAD3-2\lab10(CLR)\lab10\bin\Debug\lab10.dll'
 WITH PERMISSION_SET = EXTERNAL_ACCESS;
 GO
 
-CREATE TYPE dbo.CurrencyExchange
-EXTERNAL NAME lab10.CurrencyExchange;
+-- Создание пользовательского CLR-типа
+CREATE TYPE dbo.LicenseData
+    EXTERNAL NAME lab10.LicenseData;
 GO
 
-CREATE PROCEDURE dbo.MoveFile
-    @SourcePath NVARCHAR(4000),
-    @DestinationPath NVARCHAR(4000),
-    @Overwrite BIT = 1
-AS EXTERNAL NAME lab10.FileOperations.MoveFile;
+-- Создание CLR-процедуры по варианту 12: чтение данных из внешнего файла
+CREATE PROCEDURE dbo.ReadLicenseFile
+    @FilePath NVARCHAR(4000)
+        AS EXTERNAL NAME lab10.FileOperations.ReadLicenseFile;
 GO
 
---1
+-- Демонстрация 1: создание тестового файла с данными лицензий
 EXEC xp_cmdshell 'mkdir C:\CLR_Test';
-EXEC xp_cmdshell 'mkdir C:\CLR_Test\Source';
-EXEC xp_cmdshell 'mkdir C:\CLR_Test\Dest';
-EXEC xp_cmdshell 'echo 123 > C:\CLR_Test\Source\file.txt';
-
-EXEC xp_cmdshell 'dir C:\CLR_Test\Source\file.txt';
-
-EXEC dbo.MoveFile 'C:\CLR_Test\Source\file.txt', 'C:\CLR_Test\Dest\file.txt', 1;
-
-EXEC xp_cmdshell 'dir C:\CLR_Test\Dest\file.txt';
-EXEC xp_cmdshell 'type C:\CLR_Test\Dest\file.txt';
 GO
 
---2
-DECLARE @usd CurrencyExchange;
-SET @usd = CurrencyExchange::Parse('USD|92,50|2024-04-25');
-SELECT @usd.GetCurrencyInfo() AS [Доллар];
+EXEC xp_cmdshell 'echo 1^|Visual Studio^|AAAA-BBBB-CCCC^|2027-01-01^|199.99 > C:\CLR_Test\licenses.txt';
+GO
+
+EXEC xp_cmdshell 'echo 2^|Microsoft Office^|DDDD-EEEE-FFFF^|2023-12-31^|149.50 >> C:\CLR_Test\licenses.txt';
+GO
+
+EXEC xp_cmdshell 'echo 3^|Adobe Photoshop^|GGGG-HHHH-IIII^|2026-06-15^|299.00 >> C:\CLR_Test\licenses.txt';
+GO
+
+-- Проверка содержимого файла средствами SQL Server
+EXEC xp_cmdshell 'type C:\CLR_Test\licenses.txt';
+GO
+
+-- Демонстрация 2: выполнение CLR-процедуры чтения файла
+EXEC dbo.ReadLicenseFile 'C:\CLR_Test\licenses.txt';
+GO
+
+-- Демонстрация 3: работа с пользовательским CLR-типом LicenseData
+DECLARE @license dbo.LicenseData;
+
+SET @license = dbo.LicenseData::Parse('1|Visual Studio|AAAA-BBBB-CCCC|2027-01-01|199.99');
+
+SELECT 
+    @license.ToString() AS LicenseAsString,
+    @license.GetLicenseInfo() AS LicenseInfo,
+    @license.IsExpired() AS IsExpired;
+GO
+
+-- Демонстрация 4: проверка просроченной лицензии
+DECLARE @expiredLicense dbo.LicenseData;
+
+SET @expiredLicense = dbo.LicenseData::Parse('2|Microsoft Office|DDDD-EEEE-FFFF|2023-12-31|149.50');
+
+SELECT 
+    @expiredLicense.ToString() AS LicenseAsString,
+    @expiredLicense.GetLicenseInfo() AS LicenseInfo,
+    @expiredLicense.IsExpired() AS IsExpired;
+GO
